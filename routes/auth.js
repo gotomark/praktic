1c0c37c297ec019a308249a68a808a96c52cc9dc
gotomark <dmitriy.yakynin@gmail.com>
2020-03-13 16:22:04 +0200

    auth jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,43 @@
-const jwt = require('express-jwt');
+const router = require('express').Router();
+const User = require('../model/User');
+const {registerValidation,loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
-const getTokenFromHeaders = (req) => {
-    const { headers: { authorization } } = req;
+router.post('/register', async (req, res)=>{
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
 
-    if(authorization && authorization.split(' ')[0] === 'Token') {
-        return authorization.split(' ')[1];
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return  res.status(400).send('Email already exists')
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+    
+    try{
+        const savedUser = await user.save();
+        res.send({user: user._id});
+    }catch (err) {
+        res.status(400).send(err)
     }
-    return null;
-};
+})
 
-const auth = {
-    required: jwt({
-        secret: 'secret',
-        userProperty: 'payload',
-        getToken: getTokenFromHeaders,
-    }),
-    optional: jwt({
-        secret: 'secret',
-        userProperty: 'payload',
-        getToken: getTokenFromHeaders,
-        credentialsRequired: false,
-    }),
-};
-module.exports = auth;
+router.post('/login',async (req, res)=>{
+    const {error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return  res.status(400).send('Email not found')
+    const validPass = await bcrypt.compare(req.body.password, user.password)   ;
+    if(!validPass) return res.status(400).send('Invalid password');
+
+    const token = jwt.sign({_id: user._id},process.env.TOKEN_SECRET);
+    res.header('auth-token',token).send(token)
+})
+
+module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req,res,next){
+    const token = req.header('auth-token');
+    if(!token) return res.status(401).send('Access denied');
+
+
+}
